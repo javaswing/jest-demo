be8a5ea1589d9950f995bffe33a7400f8041a497
javaSwing <zxdlovejava@gmail.com>
2023-08-09 01:01:29 +0800

    feat: add beforeEach

--- test/token.test.ts
+++ test/token.test.ts
@@ -1,26 +1,37 @@
 import { TOKEN_KEY, getToken, setToken } from "../src/token";
 
-describe("storage test", () => {
+const reLocalStore = window.localStorage;
 
-  
+describe("storage test", () => {
+  let setItemMock: jest.Mock<any, any, any> | null;
+  let getItemMock;
+
+  beforeEach(() => {
+    setItemMock = jest.fn();
+    getItemMock = jest.fn().mockReturnValue(null);
+
+    Object.defineProperty(window, "localStorage", {
+      value: {
+        setItem: setItemMock,
+        getItem: getItemMock
+      },
+      writable: true,
+    });
+  });
 
   test("getToken Method", () => {
     const v = getToken();
     expect(v).toBeNull();
   });
 
-  test("setToken Method", () => {
-    let setItemMock = jest.fn();
-
-    Object.defineProperty(window, 'localStorage', {
-      value: {
-        setItem: setItemMock
-      }
-    })
-
+  test("setToken Method", () => {  
     setToken("name");
-
-    expect(setItemMock).toHaveBeenCalledWith(TOKEN_KEY, "name")
+    expect(setItemMock).toHaveBeenCalledWith(TOKEN_KEY, "name");
     expect(setItemMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("value is empty", () => {   
+    setToken();
+    expect(setItemMock).not.toHaveBeenCalled();
   });
 });
